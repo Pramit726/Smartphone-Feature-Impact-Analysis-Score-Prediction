07e055820fca3120dcef7162f0f922921f85d96a
Pramit726 <pramitde726@gmail.com>
2025-03-09 19:45:12 +0530

    chore: update project description in pyproject.toml

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 setup(
-    name="src",
+    name="ml",
     version="0.0.1",
     author="Pramit De",
     author_email="pramitde726@gmail.com",
-    packages=find_packages()
+    packages=find_packages(include=['ml'])
 )
