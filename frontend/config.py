efc710e070c93153ef7658aec18f649949deb4c8
Pramit726 <pramitde726@gmail.com>
2025-03-09 21:21:21 +0530

    test: CICD 2

--- frontend/config.py
+++ frontend/config.py
@@ -3,4 +3,4 @@ import os
 
 API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 logging.info(f"API_BASE_URL: {API_BASE_URL}")
-logging.info("Loaded config.py sucessfully")
+logging.info("Loaded config.py")
