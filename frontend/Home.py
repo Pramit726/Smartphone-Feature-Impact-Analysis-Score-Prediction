fc995f2f054e69d7f257fc792b4374e1f086f403
Pramit De <149934842+Pramit726@users.noreply.github.com>
2025-04-15 21:10:38 +0530

    test : remove test message in Home.py

--- frontend/Home.py
+++ frontend/Home.py
@@ -26,7 +26,7 @@ def main():
         st.stop()
 
     # Hero Section
-    st.title("📱 Understand & Predict Smartprix Ratings - Test5")
+    st.title("📱 Understand & Predict Smartprix Ratings")
     st.subheader(
         "Gain insights into how smartphone features impact specification scores and optimize your designs."
     )
