05c9625d5862d1e93707b1c423062c9392d6ff47
Pramit De <149934842+Pramit726@users.noreply.github.com>
2025-04-15 21:19:57 +0530

    test : remove test message in Home.py

--- frontend/config.py
+++ frontend/config.py
@@ -3,4 +3,4 @@ import os
 
 API_BASE_URL = os.getenv("API_BASE_URL", "http://127.0.0.1:8000")
 logging.info(f"API_BASE_URL: {API_BASE_URL}")
-logging.info("Loaded config.py")
+logging.info("Succesfully Loaded config.py")
